b41090f27c2fce9b1092195367ca90cef0c95446
Mohamed Askaf <87464421+askaf11@users.noreply.github.com>
2022-12-13 10:59:42 +0530

    Test V- 1.0

---
+++ function.js
@@ -0,0 +1,27 @@
+function getInfo() {
+    var http = new XMLHttpRequest();
+    http.open("GET", "http://askaf11.github.io/test/data.json", false);
+    http.send();
+
+    if (http.readyState == 4 && http.status == 200) {
+        var result = http.responseText;
+        var obj = JSON.parse(result);
+        var txt = "";
+        for (var i = 0; i <= 4; i++) {
+            txt += "<tr>" +
+                "<td>" + obj.users[i].username + "</td>" +
+                "<td>" + obj.users[i].age + "</td>" +
+                "<td>" + obj.users[i].gender + "</td>" +
+                "<td>" + obj.users[i].mail + "</td>" +
+                "<td>" + obj.users[i].phone + "</td>"
+            "</tr>";
+        }
+        document.getElementById("id").innerHTML = "<tr>" +
+            "<th>" + "UserName" + "</th>" +
+            "<th>" + "Age" + "</th>" +
+            "<th>" + "Gender" + "</th>" +
+            "<th>" + "Mail" + "</th>" +
+            "<th>" + "Phone" + "</th>" +
+            "</tr>" + txt;
+    };
+}
